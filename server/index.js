c9911bd5cc08c15fe44703bfed1b877cbe09086f
cssmith07060 <cssmith07060@gmail.com>
2022-03-14 07:29:43 -0400

    commit

--- server/index.js
+++ server/index.js
@@ -2,3 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose'
 import cors from 'cors'
+
+const app = express()
+
+app.use(bodyParser.json({limit:"32mb", extended: true}))
