0ea99f03d8ae3342aa1faf6f68f57ae67c5be219
cssmith07060 <cssmith07060@gmail.com>
2022-05-20 20:37:23 -0400

    bayek

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Container, AppBar, Typography, Grow, Grid} from "material-ui/core"
-
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
 import instaverse from './images/Instaverse.png'
 
 const App = () => {
@@ -11,6 +12,18 @@ const App = () => {
                 <img src={instaverse} alt="instaverse"/>
 
             </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid Container justify="space-between" alignItems="stretch" spacinig="4">
+                        <Grid item xs={12} sm={7}>
+                          <posts/>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                          <form/>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
         </Container>
     )
 }
