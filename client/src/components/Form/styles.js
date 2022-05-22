a5abc646fdb65684a0aa2bc8472f31aa159d2742
cssmith07060 <cssmith07060@gmail.com>
2022-05-21 21:16:27 -0400

    pyt

---
+++ client/src/components/Form/styles.js
@@ -0,0 +1,14 @@
+import { makeStyles } from "@material-ui/core";
+
+export default makeStyles(() => ({
+    appBar: {
+        borderRadius: 15,
+        margin: '30px 0',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#efefef'
+    },
+    
+}))
