57859f8ded59475b096f0823cef738939f357cfd
cssmith07060 <cssmith07060@gmail.com>
2022-06-13 06:51:51 -0400

    only built for cuban linx

---
+++ client/reducers/index.js
@@ -0,0 +1,12 @@
+export default reducer = (posts=[], action) => {
+    
+        switch (action.type) {
+            case "FETCH_ALL":
+                return posts;
+            case "CREATE":
+                return posts;
+            
+            default:
+                return posts;
+        }
+    }
