4d3e2ae6fb04efac290392f1ea895818b2c3de4d
cssmith07060 <cssmith07060@gmail.com>
2022-09-07 19:27:21 -0400

    mecca&thesoulbrotha

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import Post from './Post/Post'
 import usestyles from './styles'
-
+cosnt state = useSelector(state => state.state)
 
 const Post = () => {
     const classes = usestyles()
-    return <h1>Post</h1>;
+    return (
+        
+    <>
+            <h1>Post</h1>;
+            <Post />
+            <Post />
+    </>
+    
+    )
 }
 
 export default Post;
