f6285825c2ffe1b17c6c2f66312c666bad47e468
cssmith07060 <cssmith07060@gmail.com>
2023-03-28 16:05:57 -0400

    nextcommit

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import usestyles from './styles'
+import (TextField, Button, Typography, Paper) from '@material-ui/core'
 
 const Form = () => {
     const classes = usestyles()
-    return <h1>Form</h1>;
+    return <Paper className={classes.paper}>
+        <form autoComplete='off' noValidate className={classes.form}></form>
+
+    </Paper>
 }
 
 export default Form;
