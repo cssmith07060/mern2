07ca64a96edc895d763ed28dac1df093c2b29bbe
cssmith07060 <cssmith07060@gmail.com>
2023-03-29 18:05:46 -0400

    commit3

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,13 +1,24 @@
+import { TextField } from '@material-ui/core'
+import { Typography } from '@material-ui/core'
 import React from 'react'
 import usestyles from './styles'
 import (TextField, Button, Typography, Paper) from '@material-ui/core'
 
 const Form = () => {
     const classes = usestyles()
-    return <Paper className={classes.paper}>
-        <form autoComplete='off' noValidate className={classes.form}></form>
 
+    const handleSubmit = () => {
+
+    }
+   
+    
+    
+    return (<Paper className={classes.paper}>
+        <form autoComplete='off' noValidate className={classes.form}></form>
+        <Typography variant="h6">Creating a Post</Typography>  
+        <TextField name="creator" variant="outlined" label="creator" fullWidth=""/>
     </Paper>
+    );
 }
 
 export default Form;
