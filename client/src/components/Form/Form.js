e01a6d9322410f2699b5d3a79946e9e103d19e49
cssmith07060 <cssmith07060@gmail.com>
2023-04-03 14:00:32 -0400

    nextcommit

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,12 +1,13 @@
 import { TextField } from '@material-ui/core'
 import { Typography } from '@material-ui/core'
-import React from 'react'
+import React, {useState} from 'react'
 import usestyles from './styles'
 import (TextField, Button, Typography, Paper) from '@material-ui/core'
 
 const Form = () => {
-    const classes = usestyles()
-
+    const[postData, setPostData] = setState({ creator:'', title:'', message:'', tags:'', selectedFile:''})
+     const classes = usestyles()
+    
     const handleSubmit = () => {
 
     }
@@ -16,7 +17,8 @@ const Form = () => {
     return (<Paper className={classes.paper}>
         <form autoComplete='off' noValidate className={classes.form}></form>
         <Typography variant="h6">Creating a Post</Typography>  
-        <TextField name="creator" variant="outlined" label="creator" fullWidth=""/>
+        <TextField name="creator" variant="outlined"
+            label="creator" fullWidth="" value={postData.creator} onChange={(e)=> setPostData({creator: e.currentTarget.value})}/>
     </Paper>
     );
 }
